ca798fc0b00b295f92f9ced4f7032c17aa379141
harsh147780 <harshchaudhary1477@gmail.com>
2025-08-19 09:55:49 +0530

    Update Demo6.java

--- Day2/Solutions/Demo6.java
+++ Day2/Solutions/Demo6.java
@@ -5,11 +5,16 @@ public class Demo6 {
 	int value ;
 	
 	void setValue(int value) {
-		
+		this.value  = value;
 	}
+	void display(){
+		System.out.println("vaue : " + value);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Demo6 obj = new Demo6();
+		obj.setValue(100);
+
+		obj.display();
 
 	}
 
